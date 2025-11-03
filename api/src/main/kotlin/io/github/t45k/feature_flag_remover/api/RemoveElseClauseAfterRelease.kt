54b7cb0741a38a0954c0a1eb92213937fb7a481d
T45K <tasktas9@gmail.com>
2025-11-03 16:07:38 +0900

    remove garbage code

--- api/src/main/kotlin/io/github/t45k/feature_flag_remover/api/RemoveElseClauseAfterRelease.kt
+++
@@ -1,5 +0,0 @@
-package io.github.t45k.feature_flag_remover.api
-
-@Retention(AnnotationRetention.SOURCE)
-@Target(AnnotationTarget.EXPRESSION)
-annotation class RemoveElseClauseAfterRelease(val target: String)
