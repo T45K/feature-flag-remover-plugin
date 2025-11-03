42739a9d336725b2603dde64458367a0a968935f
T45K <tasktas9@gmail.com>
2025-11-03 12:12:58 +0900

    add failing test

--- src/test/resources/After.kt
+++ src/test/resources/After.kt
@@ -20,6 +20,31 @@ class Sample(
         val b = "enabled"
 
         val c = "enabled"
+
+        // conditions in when expression
+        val d = when ("a") {
+            -> "a"
+            else -> "b"
+        }
+
+        val e = when ("a") {
+            is String ->
+            else -> "b"
+        }
+
+        val f = when ("a") {
+            is String ->
+
+            else -> "b"
+        }
+
+        val g = when ("a") {
+            is String -> {
+
+            }
+
+            else -> "b"
+        }
     }
 
 
